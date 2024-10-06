1d19b56629b6163e0dbbf4eb00f8297e91aa87fd
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-06 16:38:27 -0500

    date format

--- Project-4-SwiftUI-better-rest/ContentView.swift
+++ Project-4-SwiftUI-better-rest/ContentView.swift
@@ -20,19 +20,44 @@ struct ContentView: View {
             step: 0.5
         )
         
+        Text(Date.now, format: .dateTime.hour().day().month().year())
+        
+        Text(Date.now.formatted(date: .long, time: .shortened))
+        
         DatePicker(
             "Please enter a date",
             selection: $wakeUp,
             in: dateFromNow()
         )
             .labelsHidden()
+      
     }
     
+    
+    // This is not good idea why have change in the calendar
+    // depending of the season, zone an more.
+    // Best idea uses `DateComponents`
     func rangeDate() -> ClosedRange<Date> {
-        let tomorrow = Date.now.addingTimeInterval(86400) // 1 day in seconds
+        // create a second Date instance set to one day in seconds from now
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        // create a range from those two
         let range = Date.now...tomorrow
         return range
     }
+    
+    func dateComponent() {
+//        var dateComponents = DateComponents()
+//        dateComponents.hour = 8
+//        dateComponents.minute = 0
+//        
+//        let date = Calendar.current.date(from: dateComponents) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        print(components)
+    }
+    
     
     func dateFromNow() -> PartialRangeFrom<Date> {
         return Date.now...
