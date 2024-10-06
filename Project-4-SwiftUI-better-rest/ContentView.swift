4cc71d7b444998d0970b90e9fad298d22f7425d9
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-06 10:29:39 -0500

    Initial Commit

---
+++ Project-4-SwiftUI-better-rest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-4-SwiftUI-better-rest
+//
+//  Created by Kevin Cuadros on 6/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-4-SwiftUI-better-rest/Project_4_SwiftUI_better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4_SwiftUI_better_restApp.swift
+//  Project-4-SwiftUI-better-rest
+//
+//  Created by Kevin Cuadros on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_4_SwiftUI_better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
